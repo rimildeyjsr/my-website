5bf511b7af709a9c964ae01aabc805d98ef02be0
Rimil Dey <rimil@springboard.com>
2020-04-27 00:32:10 +0530

    Updates

---
+++ precache-manifest.c9ad0bc01c27fa65a322dafc8443ec2b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e14ba63fde0042bc5d23c80b1961c46",
+    "url": "/my-website/index.html"
+  },
+  {
+    "revision": "fae9de12b2d57ca056f1",
+    "url": "/my-website/static/css/main.2b38cf82.chunk.css"
+  },
+  {
+    "revision": "a65cfc8a2e758d9de4b6",
+    "url": "/my-website/static/js/2.f2c3db9d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-website/static/js/2.f2c3db9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fae9de12b2d57ca056f1",
+    "url": "/my-website/static/js/main.6ad0d9d1.chunk.js"
+  },
+  {
+    "revision": "1afe37284f853fee8869",
+    "url": "/my-website/static/js/runtime-main.51f3dcd3.js"
+  },
+  {
+    "revision": "79260e5b693fac2ee373b659f01a2bdf",
+    "url": "/my-website/static/media/Inter-Bold.79260e5b.woff"
+  },
+  {
+    "revision": "aed27700d84e327fda56b4a427b03061",
+    "url": "/my-website/static/media/Inter-Bold.aed27700.woff2"
+  },
+  {
+    "revision": "7a8cc7241f766a142e15b2948804e547",
+    "url": "/my-website/static/media/Inter-Medium.7a8cc724.woff"
+  },
+  {
+    "revision": "f6cf0a0bc5fce3307e2c426eb14eb752",
+    "url": "/my-website/static/media/Inter-Medium.f6cf0a0b.woff2"
+  },
+  {
+    "revision": "4dd66a113d54a7f9a1ae913049610617",
+    "url": "/my-website/static/media/Inter-Regular.4dd66a11.woff2"
+  },
+  {
+    "revision": "7c539936c4c8c822b59a1bcc6c08a6ec",
+    "url": "/my-website/static/media/Inter-Regular.7c539936.woff"
+  },
+  {
+    "revision": "1db6c55ca0f2a97fbce3b0b3903a7bb7",
+    "url": "/my-website/static/media/Inter-SemiBold.1db6c55c.woff"
+  },
+  {
+    "revision": "dd8a55ef7058cdaeb96ef9fc65344726",
+    "url": "/my-website/static/media/Inter-SemiBold.dd8a55ef.woff2"
+  },
+  {
+    "revision": "548ab9b0660148361039c42473dbe82e",
+    "url": "/my-website/static/media/rimil.548ab9b0.jpg"
+  }
+]);
