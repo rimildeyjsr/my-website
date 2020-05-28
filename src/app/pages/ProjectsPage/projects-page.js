6896b2a58bcd040645de93249d95dcf335bd8786
Rimil Dey <rimil@springboard.com>
2020-05-28 18:34:03 +0530

    Projects page - change layout to accommodate new laptop mocks Add all images

--- src/app/pages/ProjectsPage/projects-page.js
+++ src/app/pages/ProjectsPage/projects-page.js
@@ -3,43 +3,48 @@ import SectionHeading from '../../components/SectionHeading/section-heading';
 import Carousel from '@brainhubeu/react-carousel';
 import '@brainhubeu/react-carousel/lib/style.css';
 import './projects-page.scss';
-import introPicture from '../../assets/rimil.jpg';
+import stAnthonyPicture from '../../assets/st-anthonys-website.png';
+import prateekPicture from '../../assets/hackertronix-website.png';
+import ticTacToePicture from '../../assets/tic-tac-toe.png';
+import toDoPicture from '../../assets/todo-website.png';
+import adminDashboardPicture from '../../assets/admin-dash.png';
+import patternsPicture from '../../assets/patterns.png';
 
 class ProjectsPage extends Component {
   projectList = [
     {
-      name: 'Patterns',
-      image: introPicture,
+      name: 'Patterns - Swift playground app',
+      image: patternsPicture,
       techStack: 'Swift',
       link: 'https://github.com/rimildeyjsr/patterns',
     },
     {
       name: 'St. Anthony School\'s Website',
-      image: introPicture,
+      image: stAnthonyPicture,
       techStack: 'HTML, CSS, Javascript, Jquery',
       link: 'http://stanthonysdoranda.org/',
     },
     {
       name: 'Prateek\'s Portfolio Website',
-      image: introPicture,
+      image: prateekPicture,
       techStack: 'HTML, CSS, Javascript, Jquery',
       link: 'https://hackertronix.com/',
     },
     {
-      name: 'ToDo List',
-      image: introPicture,
+      name: 'To-Do App',
+      image: toDoPicture,
       techStack: 'React',
       link: 'https://rimildeyjsr.github.io/todo/',
     },
     {
       name: 'Tic-tac-toe Game',
-      image: introPicture,
+      image: ticTacToePicture,
       techStack: 'Angular',
       link: 'https://rimildeyjsr.github.io/tic-tac-toe/',
     },
     {
       name: 'Admin Dashboard',
-      image: introPicture,
+      image: adminDashboardPicture,
       techStack: 'React, Firebase',
       link: 'https://github.com/rimildeyjsr/admin-dashboard',
     },
